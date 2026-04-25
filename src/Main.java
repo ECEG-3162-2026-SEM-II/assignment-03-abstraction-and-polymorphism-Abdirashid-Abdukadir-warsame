f57705d81e74152daf2ec6e8f8800d3840545733
Abdirashid-Abdukadir-warsame <enwarkomad@gmail.com>
2026-04-25 20:48:18 +0300

    deleting unnecessary files

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,1 @@
-import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-
-        ArrayList<SmartDevice> devices = new ArrayList<>();
-
-        devices.add(new SmartLight("Living Room Light"));
-        devices.add(new SmartThermostat("Hallway Thermostat"));
-
-        for (SmartDevice device : devices) {
-            device.activate();
-        }
-    }
-}
